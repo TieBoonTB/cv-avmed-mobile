19ece0d9dbcfce3b04f8ea9ccee3ce9cc455a85b
dragonstonehafiz <dragonstonehafiz@gmail.com>
2025-10-09 11:10:26 +0800

    chore: removed unused hardware info page

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'guide_page.dart';
 import 'camera_test_page.dart';
 import 'test_launcher_page.dart';
-import 'device_info_page.dart';
 
 class LandingPage extends StatelessWidget {
   Widget _buildCameraTestButton(BuildContext context) {
@@ -67,43 +66,6 @@ class LandingPage extends StatelessWidget {
     );
   }
 
-  Widget _buildDeviceInfoButton(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: 56,
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const DeviceInfoPage()),
-          );
-        },
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.white.withValues(alpha: 0.1),
-          foregroundColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-            side: BorderSide(color: Colors.white.withValues(alpha: 0.3)),
-          ),
-          elevation: 4,
-        ),
-        child: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.smartphone, size: 20),
-            SizedBox(width: 8),
-            Text(
-              'Device Information',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
